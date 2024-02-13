45a45cf5c1510b3febbcd4f4de131ba83014bb45
GarauOR <luca91garau@gmail.com>
2024-02-13 04:46:46 +0000

    Point made unique per each number
    you could add the point to a number multiple times without uniqueness
    resulting into an error when trying to make operations

--- app.js
+++ app.js
@@ -1,86 +1,106 @@
 const btnList = document.querySelectorAll(".column-container > div");
 const display = document.querySelector("#display");
 
-btnList.forEach(btn => {
-    btn.addEventListener("click", () => {
-        let isDisplayable = ["C", "DEL", "="];
-        if (!isDisplayable.includes(btn.textContent)) {
-            if(!isNaN(btn.textContent)){display.value += btn.textContent }
+btnList.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    let isDisplayable = ["C", "DEL", "="];
+    if (!isDisplayable.includes(btn.textContent)) {
+      if (!isNaN(btn.textContent)) {
+        display.value += btn.textContent;
+      }
 
-            else if (btn.textContent === ".") {
-                if (display.value[display.value.length-1] === " " ||
-                display.value.length < 1){
-                    display.value = display.value + "0" + btn.textContent
-                }
-                else if (!isNaN(display.value[display.value.length-1])) {display.value += btn.textContent}
-
-            }       
-            else {
-                if (display.value[display.value.length-1] !== " ") 
-                {display.value = display.value + " " + btn.textContent + " "};    
-        }}
-        else {
-            switch (btn.textContent) {
-                case "C":
-                    display.value = "";
-                    break;
-
-                case "DEL":
-                    if (display.value[display.value.length-1] === " ")
-                    {display.value = display.value.slice(0, display.value.length-3);}
-                    else {display.value = display.value.slice(0, display.value.length-1);}
-                    break;
-                
-                case "=":
-                    operation(display.value);
-                    break;
-            }
+      else if (btn.textContent === ".") {
+        if (onePoint(display.value)) {
+          if (
+            display.value[display.value.length - 1] === " " ||
+            display.value.length < 1
+          ) {
+            display.value = display.value + "0" + btn.textContent;
+          } else {
+            display.value += btn.textContent;
+          }
         }
-    });
+      } 
+      
+      else if (
+        display.value.length > 1 &&
+        display.value[display.value.length - 1] !== " "
+      ) {
+        display.value = display.value + " " + btn.textContent + " ";
+      }
+
+    } else {
+      switch (btn.textContent) {
+        case "C":
+          display.value = "";
+          break;
+
+        case "DEL":
+          if (display.value[display.value.length - 1] === " ") {
+            display.value = display.value.slice(0, display.value.length - 3);
+          } else {
+            display.value = display.value.slice(0, display.value.length - 1);
+          }
+          break;
+
+        case "=":
+          operation(display.value);
+          break;
+      }
+    }
+  });
 });
 
 //replace 000 with square root
 
-function operation (operationString) {
-    
-    if (operable(operationString)) {
-        let operationArr = operationString.split(" ");
-        console.log(operationArr);
-        let index = 0;
+function operation(operationString) {
+  if (operable(operationString)) {
+    let operationArr = operationString.split(" ");
+    console.log(operationArr);
+    let index = 0;
 
-        while (index < operationArr.length) {
-            if (operationArr[index] === "/" || operationArr[index] === "*") {
-                if (operationArr[index] === "/") {
-                    const division = operationArr[index-1] / operationArr[index+1];
-                    operationArr.splice(index-1,3,`${division}`);
-                    index--
-                }
-                if (operationArr[index] === "*") {
-                    const multiplication = operationArr[index-1] * operationArr[index+1];
-                    operationArr.splice(index-1,3,`${multiplication}`);
-                    index--
-                }
-            }
-            index++
+    while (index < operationArr.length) {
+      if (operationArr[index] === "/" || operationArr[index] === "*") {
+        if (operationArr[index] === "/") {
+          const division = operationArr[index - 1] / operationArr[index + 1];
+          operationArr.splice(index - 1, 3, `${division}`);
+          index--;
         }
-        
-        
-
-
-        // console.log(operationArr);
-        //la funzione fa il return del risultato
+        if (operationArr[index] === "*") {
+          const multiplication =
+            operationArr[index - 1] * operationArr[index + 1];
+          operationArr.splice(index - 1, 3, `${multiplication}`);
+          index--;
+        }
+      }
+      index++;
     }
+
+    // console.log(operationArr);
+    // aggiungere la somma e la differenza allo stesso modo della moltiplicazione e divisione
+    //la funzione fa il return del risultato
+  }
 }
 
-function operable (value) {
-    const operableCheck = ["/", "*", "-", "+"];
-    let operableResult = false;
+function operable(value) {
+  const operableCheck = ["/", "*", "-", "+"];
+  let operableResult = false;
 
-    operableCheck.forEach(item => {
-        if (value.includes(item)) {
-            operableResult = true;
-        }
-    })
+  operableCheck.forEach((item) => {
+    if (value.includes(item)) {
+      operableResult = true;
+    }
+  });
 
-    return operableResult;
+  return operableResult;
+}
+
+function onePoint(checkString) {
+  const checkArr = checkString.split(" ");
+  console.log(checkArr);
+  if (checkArr[checkArr.length - 1].includes(".")) {
+    return false;
+  } else {
+    return true;
+  }
 }
